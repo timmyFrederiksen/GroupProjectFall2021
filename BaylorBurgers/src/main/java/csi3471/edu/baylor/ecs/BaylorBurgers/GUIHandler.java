b2a3baf1d807e479b1c1c121feeae1b44a4d7caa
timmyFrederiksen <timmyfrederiksen@gmail.com>
2021-10-27 20:12:42 -0500

    NEW
    NEW

--- BaylorBurgers/src/main/java/csi3471/edu/baylor/ecs/BaylorBurgers/GUIHandler.java
+++ BaylorBurgers/src/main/java/csi3471/edu/baylor/ecs/BaylorBurgers/GUIHandler.java
@@ -1,33 +1,49 @@
 package csi3471.edu.baylor.ecs.BaylorBurgers;
 
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JPasswordField;
-import javax.swing.JTextField;
+import javax.swing.JScrollPane;
 
 public class GUIHandler {
 
 	private JFrame frame;
-	private JPanel panel;	
+	private JPanel panelMenu;	
 	
 	public void startGUI() {
+		
+		// Hard coded in the image paths and button text only bc i can, sue me
+		
+		String paths[] = {"./target/resources/fries.png", "./target/resources/hot-dog.png",
+				"./target/resources/hamburger.png", "./target/resources/taco.png",
+				"./target/resources/pizza.png"};
+		
+		String names[] = {"McFries", "McHot McDog", "McSuperBurger", "McTaco", "McPizza"};
 
 		frame = new JFrame("Menu");
 		frame.setSize(500, 500);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
 
-		panel = new JPanel();
-		frame.add(panel);
-		panel.setLayout(null);
-
-		JLabel label = new JLabel("Items:");
-		label.setBounds(10, 20, 80, 25);
-		panel.add(label);
-
+		panelMenu = new JPanel(new GridLayout(0, 2));
+		panelMenu.setBounds(0, 0, 200, 500);
+		frame.add(panelMenu);
 		
+		frame.add(new JScrollPane(panelMenu), BorderLayout.CENTER);
+		List<JButton> itemButtons = new ArrayList<JButton>();
+		for(int i = 0; i < paths.length; i++) {
+		    ImageIcon image = new ImageIcon(paths[i]);
+		    JButton button = new JButton(image);
+		    button.setText(names[i]);
+		    itemButtons.add(button);
+		    panelMenu.add(button);
+		}
 		
 		
 		frame.setVisible(true);
--- BaylorBurgers/src/main/java/csi3471/edu/baylor/ecs/BaylorBurgers/Menu.java
+++ BaylorBurgers/src/main/java/csi3471/edu/baylor/ecs/BaylorBurgers/Menu.java
@@ -1,5 +1,15 @@
 package csi3471.edu.baylor.ecs.BaylorBurgers;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Menu {
+	
+	Set<FoodDescription> items;
+	
+	public Menu() {
+		items = new HashSet<FoodDescription>(); 
+	}
+	
 
 }
