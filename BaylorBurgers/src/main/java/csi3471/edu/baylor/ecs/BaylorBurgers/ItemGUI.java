a25b34eb486146a1d98a43e69cb36e058140e8de
FrancisBoyle11 <91507837+FrancisBoyle11@users.noreply.github.com>
2021-11-28 01:02:35 -0600

    Added new screens to program. New Start main is LoginGUI.

---
+++ BaylorBurgers/src/main/java/csi3471/edu/baylor/ecs/BaylorBurgers/ItemGUI.java
@@ -0,0 +1,104 @@
+package csi3471.edu.baylor.ecs.BaylorBurgers;
+
+import javax.swing.*;
+import javax.swing.border.Border;
+import javax.swing.border.EmptyBorder;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+public class ItemGUI extends JDialog {
+
+    private FoodDescription itemDescription;
+
+    String[] labelTitles = new String[]{"Item Name: ", "Item Price:"};
+    String[] itemLabels;
+    private ArrayList<JLabel> titleLabels;
+    private ArrayList<JLabel> detailsLabels;
+
+
+    public ItemGUI(FoodDescription itemDescription){
+        this.itemDescription = itemDescription;
+        itemLabels = new String[]{itemDescription.getName(), itemDescription.getPrice().toString()};
+
+        titleLabels = new ArrayList<>();
+
+        for(int i = 0; i < labelTitles.length; i++){
+            JLabel label = new JLabel(labelTitles[i] + "   " + itemLabels[i]);
+            //label.setPreferredSize(new Dimension(100, 20));
+            titleLabels.add(label);
+        }
+        detailsLabels = new ArrayList<>();
+        /*JLabel itemName =  new JLabel(itemDescription.getName());
+        itemName.setPreferredSize(new Dimension(100, 20));
+        detailsLabels.add(itemName);
+        JLabel itemPrice =  new JLabel(itemDescription.getPrice().toString());
+        itemPrice.setPreferredSize(new Dimension(100, 20));
+        detailsLabels.add(itemPrice);*/
+
+        createAndShowGUI();
+    }
+    private void addGUIComponents() {
+        //GridLayout detailsGrid = new GridLayout(2, 2);
+        //JPanel detailsPanel = new JPanel();
+        //detailsPanel.setBorder(new EmptyBorder(new Insets(0, 25, 0, 0)));
+        //detailsPanel.setPreferredSize(new Dimension(10, 10));
+        //detailsPanel.setLayout(detailsGrid);
+
+        /*for(int i = 0; i < titleLabels.size(); i++){
+            Border border = BorderFactory.createLineBorder(Color.BLACK);
+            titleLabels.get(i).setBorder(border);
+            detailsLabels.get(i).setBorder(border);
+            detailsPanel.add(titleLabels.get(i));
+            detailsPanel.add(detailsLabels.get(i));
+        }*/
+
+        JPanel descriptionPanel = new JPanel();
+
+        BoxLayout boxLayout = new BoxLayout(descriptionPanel, BoxLayout.Y_AXIS);
+
+        descriptionPanel.setLayout(boxLayout);
+
+        JLabel descriptTitleLabel = new JLabel("Item Description:");
+        JLabel descriptionLabel = new JLabel(itemDescription.getDetails());
+        JButton orderButton = new JButton("Order " + itemDescription.getName());
+
+        descriptionPanel.setBorder(new EmptyBorder(new Insets(50, 25, 25, 0)));
+
+
+        for(JLabel label : titleLabels){
+            descriptionPanel.add(label);
+            descriptionPanel.add(Box.createRigidArea(new Dimension(0, 30)));
+        }
+        descriptionPanel.add(descriptTitleLabel);
+        descriptionPanel.add(Box.createRigidArea(new Dimension(0, 10)));
+        descriptionPanel.add(descriptionLabel);
+        descriptionPanel.add(Box.createRigidArea(new Dimension(0, 100)));
+        descriptionPanel.add(orderButton);
+
+        //GridLayout itemGridLayout = new GridLayout(2, 1);
+        //this.setLayout(itemGridLayout);
+        //add(detailsPanel);
+        add(descriptionPanel);
+
+        orderButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                dispose();
+                PurchaseItemGUI purchaseItemGUI = new PurchaseItemGUI();
+            }
+        });
+    }
+    private void createAndShowGUI() {
+        this.setTitle("Baylor Burgers " + itemDescription.getName());
+        this.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+        this.setResizable(false);
+
+        addGUIComponents();
+
+        //this.pack();
+        this.setSize(400, 450);
+        this.setVisible(true);
+    }
+}
