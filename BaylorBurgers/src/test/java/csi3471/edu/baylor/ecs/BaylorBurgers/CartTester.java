07a87576f76488998f9e331724a51c74ba87dd51
timmyFrederiksen <timmyfrederiksen@gmail.com>
2021-12-08 16:24:35 -0600

    JUNIT - Cart

---
+++ BaylorBurgers/src/test/java/csi3471/edu/baylor/ecs/BaylorBurgers/CartTester.java
@@ -0,0 +1,32 @@
+package csi3471.edu.baylor.ecs.BaylorBurgers;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import csi3471.edu.baylor.ecs.BaylorBurgers.Business.Cart;
+import csi3471.edu.baylor.ecs.BaylorBurgers.Business.CartItem;
+import csi3471.edu.baylor.ecs.BaylorBurgers.Business.FoodDescription;
+
+
+public class CartTester {
+	
+	protected Cart c = null;
+	protected FoodDescription fd = new FoodDescription("Name", "testCategory", 12.00, "none");
+	protected CartItem testItem = new CartItem(fd, 1, "no notes", 12.00);
+	
+	@Test
+	public void testCreate() {
+		c = new Cart();
+		assertTrue(c != null);
+	}
+	
+	@Test
+	public void testCartAdd() {
+		c = new Cart();
+		c.addItem(testItem);
+		assertTrue(c.getItems().contains(testItem));
+	}
+	
+}
+
